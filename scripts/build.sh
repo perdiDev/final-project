03d44f7e5767351654d9e706e13957375f857b8e
perdidev <perdidev@gmai.com>
2026-07-13 15:29:41 +0800

    build script command

---
+++ scripts/build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# 1. Hapus folder build jika sudah ada sebelumnya
+rm -rf build
+
+# 2. Buat folder build baru
+mkdir -p build && cd build
+
+# 3. Jalankan CMake
+cmake ..
+
+# 4. Jalankan kompilasi (Make) menggunakan semua core CPU yang tersedia
+make -j$(nproc)
+
+# 5. Pindahkan file hasil build (yang berawalan "Deep") ke direktori parent
+mv Deep* ../
+
+# 6. Kembali ke direktori sebelumnya
+cd ..
+
+echo "Build berhasil diselesaikan!"
